280dcc02ab61bb5bdd667e753d816fac24d726e5
mio256 <reiya@woorth.io>
2022-11-24 03:30:59 +0900

    :sparkles: hello

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print('hello')
+import os
+import tweepy
+import requests
+
+client = tweepy.Client(
+    os.environ['BEARER_TOKEN'],
+    consumer_key=os.environ['CONSUMER_KEY'],
+    consumer_secret=os.environ['CONSUMER_SECRET'],
+    access_token=os.environ['ACCESS_TOKEN'],
+    access_token_secret=os.environ['ACCESS_TOKEN_SECRET'],
+    return_type=requests.Response,
+    wait_on_rate_limit=True,
+)
+
+client.create_tweet(text='hello, world')
